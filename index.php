091d6977801785aa4dce0078638d2d27da7545c1
MerrittofD4 <merritt@d4webdesign.com>
2017-03-07 11:50:22 -0800

    testing gitignore

--- index.php
+++ index.php
@@ -5,13 +5,13 @@
 	GitHub Plugin URI: https://github.com/d4advancedmedia/d4-pizza
 	GitHub Branch: master
 	Description: Every site needs extra cheese, robust sauce, toppings, and choice of dipping sauce.
-	Version: 7Mar17c
+	Version: 7Mar17e
 	Author: D4 Adv. Media
 	License: GPL2
 */
 
 // Update this version number in the description area as well for cache busting. Use this variable as the version number in registers for scripts and styles so that a single change to this number will force a version refresh for all Arsenal files. You can still version the js and css files separately, this just denotes the version of the plugin as a whole.
-$d4pizza_version = '7Mar17c';
+$d4pizza_version = '7Mar17e';
 
 //Plugin includes
 include ('config.php');
