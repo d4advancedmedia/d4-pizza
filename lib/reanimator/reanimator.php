bcc2e1b0e6ee963c4d7ea0742078542ef293d96a
MerrittofD4 <merritt@d4webdesign.com>
2017-03-13 09:58:38 -0700

    1.1.1
    Fixed reanimator registers

--- lib/reanimator/reanimator.php
+++ lib/reanimator/reanimator.php
@@ -3,7 +3,7 @@
 /*
   Feature Name: reanimator
   Usage: Add classes to elements to create prebuilt animation effects.
-  Version: 1.0.0
+  Version: 1.0.1
   Author: D4 Adv. Media
   License: GPL2
 */
@@ -13,8 +13,8 @@ global $d4pizza_version;
 // Register and enqueue font-end plugin style sheets and scripts.
 add_action( 'wp_enqueue_scripts', 'register_d4reanimator_elements' );
 function register_d4reanimator_elements() {
-	wp_register_style( 'reanimator', plugins_url( 'lib/reanimator/d4-reanimator.css' , __FILE__ ), '', $d4pizza_version );
+	wp_register_style( 'reanimator', plugins_url( 'd4-reanimator.css' , __FILE__ ), '', $d4pizza_version );
 	wp_enqueue_style( 'reanimator' );
-	wp_register_script( 'reanimator', plugins_url( 'lib/reanimator/d4-reanimator.js' , __FILE__ ), array( "jquery" ), $d4pizza_version, true );
+	wp_register_script( 'reanimator', plugins_url( 'd4-reanimator.js' , __FILE__ ), array( "jquery" ), $d4pizza_version, true );
 	wp_enqueue_script('reanimator');
 }
